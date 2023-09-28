0137734046cf5b7b9ff37549ff81735f6355ffe5
Tedo Manvelidze <67691418+ted3x@users.noreply.github.com>
2023-09-28 22:24:32 +0400

    make RevoltFetchSessionApiResponse Serializable

--- src/commonMain/kotlin/app/revolt/model/auth/session/response/RevoltFetchSessionApiResponse.kt
+++ src/commonMain/kotlin/app/revolt/model/auth/session/response/RevoltFetchSessionApiResponse.kt
@@ -2,7 +2,9 @@ package app.revolt.model.auth.session.response
 
 import app.revolt.utils.RevoltApiConstants
 import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class RevoltFetchSessionApiResponse(
     @SerialName(RevoltApiConstants.ID)
     val id: String,
